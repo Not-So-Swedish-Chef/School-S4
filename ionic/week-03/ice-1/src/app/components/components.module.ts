91f0f74cf7f88e0bb7270963fb75e894927099a0
vd.evan <evan.vanduzer@aol.com>
2024-11-01 03:12:56 -0400

    week 3 ice 1 complete

--- ionic/week-03/ice-1/src/app/components/components.module.ts
+++ ionic/week-03/ice-1/src/app/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Comp1Component } from './comp1/comp1.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations : [ Comp1Component ],
+  imports : [
+    CommonModule,
+    FormsModule,
+    IonicModule
+  ],
+  exports : [ Comp1Component ]
 })
 export class ComponentsModule { }
--- ionic/week-03/ice-1/src/app/home/home.page.ts
+++ ionic/week-03/ice-1/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router : Router) {}
+
+  goPage1() {
+    this.router.navigate(['/page1'])
+        .then(
+          nav => { console.log(nav); },
+          err => { console.log(err); }
+        );
+  }
+
+  goPage2() {
+    this.router.navigate(['/page2'])
+        .then(
+          nav => { console.log(nav); },
+          err => { console.log(err); }
+        );
+  }
 
 }
