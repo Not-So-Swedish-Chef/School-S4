a573d468062198b32b95dea39d840aaf7608733c
vd.evan <133128349+Not-So-Swedish-Chef@users.noreply.github.com>
2024-11-22 08:21:27 -0500

    assignment 2 ionic project generation

---
+++ ionic/assignments/a2-vanduzee/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'a2-vanduzee',
+  webDir: 'www'
+};
+
+export default config;
