279f2852f2432f4dfbd7ddde5cc0d260662423a1
vd.evan <evan.vanduzer@aol.com>
2024-11-26 17:31:29 -0500

    a2 - figured out page navigation

--- ionic/assignments/a2/a2-vanduzee/src/app/app-routing.module.ts
+++ ionic/assignments/a2/a2-vanduzee/src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'pages/main',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'pages/main',
+    loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'pages/listing',
+    loadChildren: () => import('./pages/listing/listing.module').then( m => m.ListingPageModule)
+  },
+  {
+    path: 'pages/update',
+    loadChildren: () => import('./pages/update/update.module').then( m => m.UpdatePageModule)
   }
 ];
 
